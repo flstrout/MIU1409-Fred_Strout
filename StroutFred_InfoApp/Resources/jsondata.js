abc05251aba111522a813680f49e334ba2817d0b
fredstrout <flstrout@cox.net>
2014-09-17 21:39:55 -0500

    Added image slider

--- StroutFred_InfoApp/Resources/jsondata.js
+++ StroutFred_InfoApp/Resources/jsondata.js
@@ -3,6 +3,10 @@
 // Info App JSON Data
 // Created On: 9/15/2014
 
+// global variables
+var gallery = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "images");
+var myImages = gallery.getDirectoryListing();
+
 // Data
 var jsonData = {
 	"Breeds": [
@@ -26,7 +30,10 @@ var jsonData = {
 			    "TEMPERAMENT":"The Rottie is powerful, calm, trainable, courageous and devoted to its owner and family. Loyal and protective, it will defend its family fiercely if needed, seemingly immune to pain. Serious, even-tempered, brave, confident and courageous, this breed needs an owner who is strong minded, calm, but firm and able to handle this dog_s massive size. The Rottie is a docile, natural guard dog with a laid-back, reliable temperament. It is highly intelligent and has proven its worth beyond question in police, military and customs work over many centuries and can be trained for competitive obedience. Because of its size, training should begin when the dog is a small puppy. This breed needs a lot of leadership and socialization. It will not be happy confined to a kennel or backyard. The objective in training this dog is to achieve pack leader status. It is a natural instinct for a dog to have an order in its pack. When we humans live with dogs, we become their pack. The entire pack cooperates under a single leader. Lines are clearly defined and rules are set. You and all other humans MUST be higher up in the order than the dog. That is the only way your relationship can be a success. When the Rottweiler receives consistent leadership and is trained, it will be a good playmate for the children. It will accept cats, other dogs and other household pets as long as the dog has been socialized well and has owners who assert their authority over the dog. Friends and relatives of the family are normally enthusiastically welcomed. Strangers from whom the dog senses bad intentions can get no further than the sidewalk.",
 			    "LINEAGE":"The Rottweiler is probably descended from the Italian Mastiff, which accompanied the herds that the Romans brought with them when they invaded Europe. During the Middle Ages, it was used as a herder, as a guard, messenger dog, draught dog and for police work. It was bred in the German town of Rottweiler in Wurttemberg. Practically extinct in the 1800s, the breed population began a comeback in the early twentieth century due to the efforts of enthusiastic breeders centered in Stuttgart. In Germany on January 13, 1907, the DRK (Deutscher Rottweiler-Klub (German Rottweiler Club)) was established. Shortly after on April 27, 1907, the SDRK (S�ddeutscher Rottweiler-Klub (South German Rottweiler Club)) was formed, which later became the IRK (International Rottweiler Club). The Rottweiler standard was then set. The breed was first recognized by the AKC in 1931. Some of the Rottweiler's talents include: tracking, herding, watchdog, guarding, search and rescue, guide dogs for the blind, police work, carting, competitive obedience and Schutzhund. ",
 			    "GROUP":"Mastif, Working",
-			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com"
+			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com",
+			    "IMAGE1":"1.png",
+			    "IMAGE2":"2.jpg",
+			    "IMAGE3":"3.jpg"
 			  },
 			  {
 			  	"TITLE":"Chihuahua",
@@ -48,7 +55,10 @@ var jsonData = {
 			    "TEMPERAMENT":"The Chihuahua is a good companion dog. Courageous, extremely lively, proud and adventurous, they enjoy affection. Brave, cheerful and agile, Chihuahuas can be strong-willed without proper human leadership. They are loyal and become attached to their owners. Some like to lick their owner's faces. Socialize them well. For some, they may be slightly difficult to train, but they are intelligent, learn quickly, and respond well to proper, firm but gentle (positive reinforcement) training. May be difficult to housebreak. Do not let the Chihuahua get away with things you would not allow a large dog to do (Small Dog Syndrome), such as jumping up on humans. While it may be cute for a 5-pound tiny dog to put his paws on your leg when you come home from work, it is allowing a dominant behavior. If you allow this little dog to be your pack leader it will develop many behavior issues such as jealousy, aggression with other dogs and sometimes with humans, and will become undeniably suspicious of people except for its owner. When strangers are present, it will begin to follow its owner's every move, keeping as close as possible. A Chihuahua that is pack leader of its humans may snap at children. This breed is generally not recommended for children, not because it is not good with them, but because most people treat the Chihuahua differently than they would a large dog, causing it to become untrustworthy. Because of its size, this breed tends to be babied and things we humans clearly see as bad behavior for a large dog are looked over as cute with a small dog. Small dogs also tend to be walked less, as humans assume they get enough exercise just running around during the day. However, a walk provides more than just exercise. It provides mental stimulation and satisfies the migration instinct all dogs have. Because of this, small breeds such as the Chihuahua tend to become snappish, yappy, protective and untrustworthy with kids and humans they do not know. Chihuahuas that are their human's pack leader tend to be fairly dog-aggressive. An owner who realizes this and treats the Chihuahua no differently than they would a large breed, becoming a clear pack leader, will get a different, more appealing temperament out of this wonderful little dog, finding it to be a good little child companion.",
 			    "LINEAGE":"This is the oldest breed on the American continent and the smallest breed in the world. Native to Mexico, where it received its name from the Mexican State of Chihuahua. It was only brought to Europe at the end of the nineteenth century. The breeds that were used to make up the Chihuahua are unclear, but some think it originated from the Fennec Fox. The dogs were sacred to Pre-Columbian Indian nations and were also popular pets to the upper class. The dogs are prized for their size and are most valued to some fanciers when they weigh under 2-1/4 pounds (1.3 kg).",
 			    "GROUP":"Southern, Toy",
-			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com"
+			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com",
+			    "IMAGE1":"4.png",
+			    "IMAGE2":"5.jpg",
+			    "IMAGE3":"6.jpg"
 			  },
 			  {
 			  	"TITLE":"Rat Terrier",
@@ -70,7 +80,10 @@ var jsonData = {
 			    "TEMPERAMENT":"The Rat Terrier is an intelligent, alert and loving dog. It is very inquisitive and lively. This affectionate dog makes an excellent companion for those who will enjoy an energetic dog. They are good with children, especially if they are raised with them from puppyhood. They are, for the most part, friendly with strangers. Rat Terriers make good watchdogs. These dogs are quick, very playful and are not yappers. The temperament of these dogs is pure terrier. The lively, feisty, fearless nature can be found in the best of terriers. They are eager to please and respond and pick training up faster than most dogs. The Rat Terrier is a very well-mannered, well-rounded dog. It is easy to train, very eager to learn and to please its owner. They love to go with you and to do what you do. They are also very good swimmers, not bashful or afraid and have no problem with the water. They make good farm dogs as well as excellent family dogs for pets and companionship. This hardy dog is used for hunting expeditions as well as terrier work. Adult dogs can adjust easily into families with or without children. Make sure you are this dog's firm, confident, consistent pack leader to avoid Small Dog Syndrome, human induced behavior problems that can include territorial issues. Always remember, dogs are canines, not humans. Be sure to meet their natural instincts as animals.",
 			    "LINEAGE":"Named by the perceptive Teddy Roosevelt, the Rat Terrier was developed in Great Britain originally from the Smooth Fox Terrier and the Manchester Terrierin 1820. It was brought to the USA in the 1890s. At that time they were all their original color of black and tan. Life Magazine showed President Roosevelt with three black and tan Rat Terriers. American breeders crossed them again with the Smooth Fox Terrier as well as the Beagle and Whippet. The Beagle increased bulk, trailing and hunting ability, along with the red color. The Whippet contributed the speed and agility and probably the blue and brindle colors. The smallest variety was derived from the Smooth Fox Terrier and Chihuahua. The Rat Terrier proved to be one of the best in the rat-baiting pits. One Rat Terrier is reported to have killed over 2,501 rats in a span of only seven hours in a rat infested barn. The Rat Terrier is a hard-working farm hand, able to rid an infested barn of vermin with no problem.",
 			    "GROUP":"Terrier",
-			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com"
+			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com",
+			    "IMAGE1":"7.png",
+			    "IMAGE2":"8.jpg",
+			    "IMAGE3":"9.jpg"
 			  },
 			  {
 			  	"TITLE":"Yorkshire Terrier (Yorkie)",
@@ -92,7 +105,10 @@ var jsonData = {
 			    "TEMPERAMENT":"Yorkshire Terriers seem oblivious of their small size. They are very eager for adventure. This little dog  is highly energetic, brave, loyal and clever. With owners who take the time to understand how to treat a small dog, the Yorkie is a wonderful companion! It is affectionate with its master, but if humans are not this dog's pack leader, it can become suspicious of strangers and aggressive to strange dogs and small animals. It can also become yappy, as the dog does their best to tell you what IT wants YOU to do. It has a true terrier heritage and needs someone who understands how to be its leader. Yorkies are often only recommended for older, considerate children, simply because they are so small, most people allow them to get away with behaviors no dog should display. This changes the dog_s temperament, as the dog starts to take over the house (Small Dog Syndrome). Yorkies that become demanding and dependent, appearing to need a lot of human attention and/or developing jealous behaviors, snapping if surprised, frightened or over-teased, have owners who need to rethink how they are treating the dog. Owners who do not instinctually meet the dog_s needs may also find them to become overprotective and become neurotic. Yorkies are easy to train, although they can sometimes be stubborn if owners do not give the dog proper boundaries. They can be difficult to housebreak. The Yorkie is an excellent watchdog. When owners display pack leadership to the Yorkshire Terrier, it is very sweet and loving and can be trusted with children. The problems only arise when owners, because of the dog_s cute little size, allow it to take over the house.",
 			    "LINEAGE":"The Yorkshire Terrier_s original function was to hunt and kill rats and other rodents in the mines and cotton mills in county Yorkshire in northern England. It is thought to trace back to a small, fairly long-coated, bluish-gray dog that typically weighed about 10 pounds, called the Waterside Terrier. The Waterside Terrier was common in the Yorkshire region and was popular with miners in the West Riding area. In the middle of the 19th century, at the height of the Industrial Revolution, Scottish weavers and other laborers migrated south to England in search of work. They brought with them their small Scottish terriers of non-descript heritage. In Yorkshire, these dogs were crossed with local terriers to create the Broken-Haired Scotch Terrier, which became well-known as a superb ratter in local textile factories and coal mines. Over time, other crosses undoubtedly occurred. Although experts cannot agree on the Yorkie_s precise ancestors, the following breeds have been suggested: the Maltese Terrier, the Skye Terrier, the Dandie Dinmont Terrier, the Waterside Terrier, the old rough-coated Black-and-Tan English Terrier, the Manchester Terrier, the Paisley Terrier, the now-extinct long-haired Leeds Terrier and the Clydesdale Terrier. The end result of whatever crosses took place eventually was called the Yorkshire Terrier. It was larger than today_s Yorkie and was tenacious enough to tackle even the largest and fiercest of rodents.",
 			    "GROUP":"Terrier, Toy",
-			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com"
+			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com",
+			    "IMAGE1":"10.png",
+			    "IMAGE2":"11.jpg",
+			    "IMAGE3":"12.jpg"
 			  },
 			  {
 			  	"TITLE":"Shih Tzu",
@@ -114,13 +130,18 @@ var jsonData = {
 			    "TEMPERAMENT":"The name Shih Tzu means _little lion,_ but these love bugs are anything but fierce and ferocious. This breed is one of the best all-around breeds: they can live in any sized home, get along well with other pets, are excellent for families, single people and make ideal companions for the elderly; they are generally well behaved, are fairly easy to train and are cute as little buttons with their unique underbite. Shih Tzu owners love this quote by American composer James Mumsford, which they think accurately depicts the Shih Tzu, _... a dash of lion, several teaspoons of rabbit, a couple of ounces of domestic cat, one part court jester, a dash of ballerina, a pinch of old man, a bit of beggar, a tablespoon of monkey, one part baby seal, (and) a dash of teddy bear._",
 			    "LINEAGE":"Sixteenth century documents and paintings show dogs resembling the Shih Tzu. The Shih Tzu is said to have descended from crossing the Lhasa Apso or Tibetan mountain dog and Pekingese, in the city of Peking in the 17th century. The dogs were favorites of the Chinese royals and were so prized that for years the Chinese refused to sell, trade, or give away any of the dogs. It was not until the 1930s that the first pair was imported to England, when it was discovered by English soldiers during World War II.",
 			    "GROUP":"Herding, Toy",
-			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com"
+			    "SOURCES":"www.dogbreedinfo.com; www.petwave.com",
+			    "IMAGE1":"13.png",
+			    "IMAGE2":"14.jpg",
+			    "IMAGE3":"15.jpg"
 			  }
 		]
 };
 
 // Open the Answer Window
 var showDetail = function(dataSource){
+	var selectedImage = dataSource.image2;
+	
 	var detailWindow = Ti.UI.createWindow({
 		backgroundColor: "#1174c1",
 		title: "The Pet Store",
@@ -244,11 +265,27 @@ var showDetail = function(dataSource){
 		left: 5,
 		right: 5
 	});
+	var labelObedience = Ti.UI.createLabel({
+		text: "Obedience:",
+		font: {fontSize: 14, fontFamily: "Helvetica Neue"},
+		color: "#000065",
+		top: dataTrainability.top + 18,
+		left: 8,
+		right: 5
+	});
+	var dataObedience = Ti.UI.createLabel({
+		text: dataSource.obedience,
+		font: {fontSize: 16, fontFamily: "Helvetica Neue", fontWeight: "bold"},
+		color: "#d6ff5f",
+		top: labelObedience.top + 14,
+		left: 5,
+		right: 5
+	});
 	var labelColor = Ti.UI.createLabel({
 		text: "Color:",
 		font: {fontSize: 14, fontFamily: "Helvetica Neue"},
 		color: "#000065",
-		top: dataTrainability.top + 18,
+		top: dataObedience.top + 18,
 		left: 8,
 		right: 5
 	});
@@ -308,27 +345,43 @@ var showDetail = function(dataSource){
 		left: 165,
 		right: 5
 	});
-	var labelObedience = Ti.UI.createLabel({
-		text: "Obedience:",
-		font: {fontSize: 14, fontFamily: "Helvetica Neue"},
-		color: "#000065",
-		top: labelWeight.top,
-		left: 168,
-		right: 5
+	var imageSlider = Ti.UI.createSlider({
+		top:labelActivityLevel.top + 2,
+		height: 2,
+		width: 185,
+		min: 0,
+		max: 2,
+		value: 1,
+		left: "40%"
 	});
-	var dataObedience = Ti.UI.createLabel({
-		text: dataSource.obedience,
-		font: {fontSize: 16, fontFamily: "Helvetica Neue", fontWeight: "bold"},
-		color: "#d6ff5f",
-		top: labelObedience.top + 14,
-		left: 165,
-		right: 5
+	
+	var imageView = Ti.UI.createImageView({
+			image: "images/" + selectedImage,
+			top: labelOrigin.top,
+			left: "40%",
+			right: 5,
+			borderRadius: 3,
+			height: 125
+		});
+		
+	imageSlider.addEventListener("touchend", function(e){
+		this.value = Math.round(e.value);
+		if (imageSlider.value === 0){
+			selectedImage = dataSource.image1;
+			} else if (imageSlider.value === 1){
+			selectedImage = dataSource.image2;
+			} else {
+			selectedImage = dataSource.image3;
+		};
+		imageView.image = "images/" + selectedImage;
 	});
+	
+	
 	var labelGuardAbility = Ti.UI.createLabel({
 		text: "Guard Ability:",
 		font: {fontSize: 14, fontFamily: "Helvetica Neue"},
 		color: "#000065",
-		top: dataObedience.top + 18,
+		top: labelTrainability.top,
 		left: 168,
 		right: 5
 	});
@@ -356,7 +409,7 @@ var showDetail = function(dataSource){
 		left: 165,
 		right: 5
 	});
-	detailWindow.add(dataBreed,dataPronunciation,labelOrigin,dataOrigin,labelLifeSpan,dataLifeSpan,labelHeight,dataHeight,labelWeight,dataWeight,labelActivityLevel,dataActivityLevel,labelTrainability,dataTrainability,labelColor,dataColor,labelCoat,dataCoat,labelPersonality,dataPersonality,labelGroup,dataGroup,labelObedience,dataObedience,labelGuardAbility,dataGuardAbility,labelWatchAbility,dataWatchAbility);
+	detailWindow.add(dataBreed,dataPronunciation,labelOrigin,dataOrigin,labelLifeSpan,dataLifeSpan,labelHeight,dataHeight,labelWeight,dataWeight,labelActivityLevel,dataActivityLevel,labelTrainability,dataTrainability,labelColor,dataColor,labelCoat,dataCoat,labelPersonality,dataPersonality,labelGroup,dataGroup,labelObedience,dataObedience,labelGuardAbility,dataGuardAbility,labelWatchAbility,dataWatchAbility,imageView,imageSlider);
 	navWindow.openWindow(detailWindow);
 };
 
@@ -395,6 +448,9 @@ var lists = [];
 			temperament:jsonData.Breeds[o].TEMPERAMENT,
 			lineage:jsonData.Breeds[o].LINEAGE,
 			sources:jsonData.Breeds[o].SOURCES,
+			image1:jsonData.Breeds[o].IMAGE1,
+			image2:jsonData.Breeds[o].IMAGE2,
+			image3:jsonData.Breeds[o].IMAGE3,
 			hasChild: true,
 			backgroundColor: "#eee",
 			color: "#014421"
